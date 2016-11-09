58452402a18da7bab87ceb058baf937fd1cc590f
Tim Z <tjkz87@gmail.com>
2016-11-08 17:25:57 -0800

    make test

--- test/main.js
+++ test/main.js
@@ -1,13 +1,17 @@
-const request = require('supertest');
-const server = require('../index');
-
-const agent = request.agent(server);
+const rp = require('request-promise');
+const expect = require('chai').expect;
 
 describe('GET /', () => {
   it('should respond with 200', (done) => {
-    agent
-      .get('/')
-      .expect(200, done);
+    rp({
+      method: 'GET',
+      uri: 'http://localhost:3001',
+      resolveWithFullResponse: true 
+    })
+      .then((response) => {
+        expect(response.statusCode).to.equal(200);
+	done();
+      });
   });
 });
 
